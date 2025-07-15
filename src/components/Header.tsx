1506b9818fb3d84384675811c400ac6412742c47
Shota-Sunada <shota.sunada.dev@gmail.com>
2025-07-15 22:31:42 +0900

    ヘッダーの境目を明確化

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,10 @@ const Header = (props: { text: string; author: string; showHome: boolean }) => {
 
   return (
     <>
-      <div className="flex flex-row">
+      <div className="flex flex-row border-b-[1px] w-full justify-center items-center mb-[2dvh]">
         {props.showHome ? (
           <p
-            className="text-xl mb-[3dvh] justify-center items-center flex mr-[5dvh] cursor-pointer"
+            className="text-xl mb-[1dvh] justify-center items-center flex mr-[5dvh] cursor-pointer"
             onClick={() => navigate("/")}
           >
             {"ホームへ"}
@@ -16,7 +16,7 @@ const Header = (props: { text: string; author: string; showHome: boolean }) => {
         ) : (
           <></>
         )}
-        <p className="text-3xl mb-[3dvh]">{props.text}</p>
+        <p className="text-3xl mb-[1dvh]">{props.text}</p>
         {/* <p className="text-xl ml-[10px] mb-[3dvh] flex items-center justify-center">
           {"作成者: "}
           {props.author}
